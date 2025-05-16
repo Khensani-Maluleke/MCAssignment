964649d6f049d6f72fdf08434600fdacb1019799
mashau01 <w.mashau2006@gmail.com>
2025-05-16 15:14:23 +0200

    <mashau>

--- app/src/main/java/com/example/mcpro/signup_or_signin.java
+++ app/src/main/java/com/example/mcpro/signup_or_signin.java
@@ -1,6 +1,7 @@
 package com.example.mcpro;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignUpForm extends AppCompatActivity {
+public class signup_or_signin extends AppCompatActivity {
+    Button sign_up;
+    Button login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_signup_or_signin);
+
+
+
     }
 }
